fef1e177df1cbe230b8e6176e5fc2c41304a5130
anacarton <anacarton.dlc@gmail.com>
2023-02-02 23:59:06 +0100

    Seguimos con Spring Security. jwt. TB HICE LA PARTE DE ENVIAR EMAILS PERO AL FINAL DA ERROR POR CERTIFICADO.MIRAR

---
+++ src/main/java/com/project/trashure/config/JwtUtils.java
@@ -0,0 +1,85 @@
+package com.project.trashure.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+    private String jwtSigningKey = "secret";
+
+    public String extraerUsername(String token) {
+        return extraerClaim(token, Claims::getSubject);
+    }
+
+    public Date extraerExpiracion(String token) {
+        return extraerClaim(token, Claims::getExpiration);
+    }
+
+    public boolean hasClaim(String token, String claimName) {
+        final Claims claims = extraerAllClaims(token);
+        return claims.get(claimName) != null;
+    }
+
+    public <T> T extraerClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extraerAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extraerAllClaims(String token) {
+        return Jwts.parser().setSigningKey(jwtSigningKey).parseClaimsJwt(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extraerExpiracion(token).before(new Date());
+    }
+
+
+    public String generarToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return crearToken(claims, userDetails);
+
+    }
+
+    public String generarToken(UserDetails userDetails, Map<String, Object> claims){
+        return crearToken(claims, userDetails);
+    }
+
+    public String crearToken(Map<String, Object> claims, UserDetails userDetails){
+        return Jwts.builder().setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .claim("authorities", userDetails.getAuthorities())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
+                .signWith(SignatureAlgorithm.HS256, jwtSigningKey).compact();
+    }
+
+    public Boolean tokenEsValido (String token, UserDetails userDetails){
+        final String username = extraerUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/project/trashure/config/SecurityConfiguration.java
+++ src/main/java/com/project/trashure/config/SecurityConfiguration.java
@@ -1,23 +1,70 @@
 package com.project.trashure.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration {
 
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User("trashureteam@gmail.com",
+                    "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User("anacarton.dlc@gmail.com",
+                    "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+    );
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         //Queremos que cualquier petición sea autenticada
         http.authorizeHttpRequests().
                 anyRequest().authenticated().
-                and().httpBasic();
+                and().
+                authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return null;
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return  new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                return APPLICATION_USERS.stream().filter(user -> user.getUsername().equals(email)).findFirst()
+                        .orElseThrow(()->  new UsernameNotFoundException("No se encontró ningún usuario"));
+            }
+        };
     }
 }
