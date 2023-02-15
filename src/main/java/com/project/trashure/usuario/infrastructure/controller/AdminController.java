91342d85c8affc9e5a16192b3362d0a6a80c442f
anacarton <anacarton.dlc@gmail.com>
2023-02-15 23:19:46 +0100

    he comprobado que no se produce inyección sql, he añadido varias vistas a admin, no todas funcionales por ahora. tengo que añadir poder editar usuarios, poder eliminarlos, poder eliminar productos, añadir imágenes a la parte de transacciones y de objetos...

--- src/main/java/com/project/trashure/usuario/infrastructure/controller/AdminController.java
+++ src/main/java/com/project/trashure/usuario/infrastructure/controller/AdminController.java
@@ -4,15 +4,20 @@ import com.project.trashure.producto.domain.Producto;
 import com.project.trashure.producto.infrastructure.repository.port.FindProductoPort;
 import com.project.trashure.transaccion.domain.Transaccion;
 import com.project.trashure.transaccion.infrastructure.repository.port.FindTransaccionPort;
+import com.project.trashure.usuario.application.port.CreateUsuarioPort;
+import com.project.trashure.usuario.application.port.UpdateUsuarioPort;
 import com.project.trashure.usuario.domain.Usuario;
 import com.project.trashure.usuario.domain.UsuarioJpa;
 import com.project.trashure.usuario.infrastructure.repository.port.FindUsuarioPort;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -26,6 +31,11 @@ public class AdminController {
     private FindUsuarioPort findUsuarioPort;
 
     private FindTransaccionPort findTransaccionPort;
+
+    private CreateUsuarioPort createUsuarioPort;
+
+    private UpdateUsuarioPort updateUsuarioPort;
+
     @GetMapping("/principal")
     public String principal(Model model){
         //el método principal recibe como parámetro un objeto Model
@@ -67,5 +77,44 @@ public class AdminController {
         //Redirige a la vista de las transacciones de Admin
         return "admin/transacciones";
     }
+
+    @PostMapping("registrarUsuario")
+    public String registrarUsuario(Usuario usuario) throws NoSuchAlgorithmException {
+
+        //El usuario que llega como parámetro viene de los datos del formulario de la vista sign_up
+        //hay que determinar que el usuario que se registra es de tipo normal, es decir, no es administrador
+
+        usuario.setTipoUsuario("USER");
+
+        //se crea el usuario con los datos aportados
+        createUsuarioPort.create(usuario);
+        //Tras guardar el nuevo usuario en la base de datos, redirige a la página principal de usuario
+        return "redirect:/api/v0/admin/principal";
+    }
+
+    @PostMapping("updateUsuario")
+    public String updateUsuario(Usuario usuario, Model model, HttpSession httpSession) throws Exception {
+        System.out.println("heyy qué pasa makinasa");
+        //Usuario usuario1 = findUsuarioPort.findById(usuario.getIdUsuario());
+
+
+        System.out.println("usuario id parametro " + usuario.getIdUsuario());
+
+        String id =  httpSession.getAttribute("idUsuario").toString();
+        Integer idInt = Integer.parseInt(id);
+        Usuario usuarioHttp = findUsuarioPort.findById(idInt);
+
+        System.out.println("usuario http id " + usuarioHttp.getIdUsuario());
+        model.addAttribute("usuarioLogged", httpSession.getAttribute("idUsuario").toString());
+        //usuario.setIdUsuario(usuario1.getIdUsuario());
+
+        //MIRAR ESTO
+        //EL UPDATE NOP ME GFUSTA. COMPARAR CON EL SUYO EN MIN 13.50 VIDEO 18
+        updateUsuarioPort.update(usuarioHttp.getIdUsuario(), usuario.getNombre(), usuario.getApellidos(), usuario.getEmail(), usuario.getTelefono(),
+                usuario.getDireccion(), usuario.getLocalidad());
+
+        return "admin/usuarios";
+    }
+
 
 }
